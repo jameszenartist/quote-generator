3a4f25a38edac1c47e96183cf731d619351a2f69
James Hansen <jameszenartist@gmail.com>
2021-10-27 00:38:08 -0400

    added button functionality & CSS changes

--- script.js
+++ script.js
@@ -1,28 +1,19 @@
-let quoteArray = [];
-let index = 0;
-// let button = document.querySelector(".btn");
 let destination = document.querySelector(".quote");
-
-window.addEventListener("load", pushQuote);
+destination.addEventListener("load", addClass);
 
 async function getQuote() {
   try {
     const response = await fetch("https://api.quotable.io/random")
       .then((response) => response.json())
       .then((data) => {
-        quoteArray[index] = data.content;
+        destination.innerHTML = `${data.content}`;
       });
-    // console.log(quoteArray[index]);
-    // console.log("Response received");
-    quoteArray[index] += " ";
-    destination.innerHTML = quoteArray[index];
     return response;
   } catch (err) {
     console.log(err);
   }
 }
 
-function pushQuote() {
-  getQuote();
-  setTimeout("pushQuote()", 7000);
+function addClass() {
+  document.querySelector("quote").classList.add("fade");
 }
