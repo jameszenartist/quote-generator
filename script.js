baa5a0967f988c53aa5e6e8e7b81ee7ed66d8bc2
James Hansen <jameszenartist@gmail.com>
2021-11-22 01:17:29 -0500

    refined upon JS algorithm & CSS fading animations

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
 let destination = document.querySelector(".quote");
 let button = document.querySelector(".btn");
+let baseTime = 9000;
+let wordCount, time;
 
 async function getQuote() {
   try {
     const response = await fetch("https://api.quotable.io/random")
       .then((response) => response.json())
       .then((data) => {
+        wordCount = data.content.split(" ").length;
+        time = wordCount * 200;
+        if (time >= baseTime) {
+          baseTime = time;
+        }
         destination.innerHTML = `${data.content}`;
       });
+    destination.style.opacity = null;
     destination.classList.add("fade");
-    setTimeout(removeClass, 9000);
+    setTimeout(removeClass, baseTime);
+
     return response;
   } catch (err) {
     console.log(err);
@@ -18,19 +27,17 @@ async function getQuote() {
 
 function removeClass() {
   destination.classList.remove("fade");
-  button.classList.remove("fadeout");
-  button.disabled = false;
-  destination.style.opacity = 0;
+  destination.classList.add("fadeout");
+  button.classList.remove("fadeoutBtn");
+
+  setTimeout(() => {
+    destination.classList.remove("fadeout");
+    destination.style.opacity = 0;
+    button.disabled = false;
+  }, 2000);
 }
 
 function btnFade() {
-  button.classList.add("fadeout");
+  button.classList.add("fadeoutBtn");
   button.disabled = true;
 }
-
-//Potential code for calc avg length of paragraph for timed fade:
-
-// const average = (...nums) =>
-//   nums.reduce((acc, val) => acc + val, 0) / nums.length;
-// average(...[1, 2, 3]); // 2
-// average(1, 2, 3); // 2
